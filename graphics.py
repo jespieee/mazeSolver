fcdbfe1cebcec40feb7f6f5dd3e9dee79f53fe81
jespieee <mhuhu@umich.edu>
2024-08-10 20:17:34 -0500

    define Point and Line classes, draw stuff!

--- graphics.py
+++ graphics.py
@@ -9,6 +9,9 @@ class Window:
         self.__running = False
         self.__rootWidget.protocol("WM_DELETE_WINDOW", self.close)
     
+    def draw_line(self, Line, color):
+        Line.draw(self.__canvasWidget, color)
+    
     def redraw(self):
         self.__rootWidget.update_idletasks()
         self.__rootWidget.update()
@@ -21,3 +24,16 @@ class Window:
     def close(self):
         self.__running = False
         
+class Point:
+    def __init__(self, x ,y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.__point1 = point1
+        self.__point2 = point2
+
+    def draw(self, Canvas, color):
+        Canvas.create_line(self.__point1.x, self.__point1.y, self.__point2.x, self.__point2.y, fill=color, width=2)
+    
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    p1 = Point(0, 500)
+    p2 = Point(50, 50)
+    ln1 = Line(p1, p2)
+    win.draw_line(ln1,"blue")
     win.wait_for_close()
 
 main()
