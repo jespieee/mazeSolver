56e724729910f579ddb4e282f8bd37c40ca4b62d
jespieee <mhuhu@umich.edu>
2024-08-10 20:59:35 -0500

    add cell class, draw some cells!

--- cell.py
+++ cell.py
@@ -1,42 +1,5 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Point, Line
 
-class Window:
-    def __init__(self, width, height):
-        self.__rootWidget = Tk()
-        self.__rootWidget.title("mazeSolver")
-        self.__canvasWidget = Canvas(self.__rootWidget, bg="White", height=height, width=width)
-        self.__canvasWidget.pack(fill=BOTH, expand=1)
-        self.__running = False
-        self.__rootWidget.protocol("WM_DELETE_WINDOW", self.close)
-    
-    def draw_line(self, Line, color):
-        Line.draw(self.__canvasWidget, color)
-    
-    def redraw(self):
-        self.__rootWidget.update_idletasks()
-        self.__rootWidget.update()
-    
-    def wait_for_close(self):
-        self.__running = True
-        while (self.__running):
-            self.redraw()
-    
-    def close(self):
-        self.__running = False
-        
-class Point:
-    def __init__(self, x ,y):
-        self.x = x
-        self.y = y
-
-class Line:
-    def __init__(self, point1, point2):
-        self.__point1 = point1
-        self.__point2 = point2
-
-    def draw(self, Canvas, color):
-        Canvas.create_line(self.__point1.x, self.__point1.y, self.__point2.x, self.__point2.y, fill=color, width=2)
-    
 class Cell:
     def __init__(self, p1, p2, window, has_left=True, has_right=True, has_top=True, has_bottom=True):
         self.has_left = has_left
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from graphics import Window, Line, Point
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    p1 = Point(0, 500)
-    p2 = Point(50, 50)
-    ln1 = Line(p1, p2)
-    win.draw_line(ln1,"blue")
+    p1 = Point(10, 10)
+    p2 = Point(20, 20)
+    cell = Cell(p1, p2, win, True, True, True, True)
+    cell.draw()
     win.wait_for_close()
 
 main()
